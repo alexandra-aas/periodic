dbb333c3a19abe7f580baae70ea8b7b4660e5966
Ali Aas <ali@thoughtbot.com>
2017-08-28 09:41:15 -0400

    Add tachyons

--- helpers/application_helpers.rb
+++ helpers/application_helpers.rb
@@ -1,26 +1,27 @@
 module ApplicationHelpers
-  def markdown(contents)
-    renderer = Redcarpet::Render::HTML
-    markdown = Redcarpet::Markdown.new(
-      renderer,
-      autolink: true,
-      fenced_code_blocks: true,
-      footnotes: true,
-      highlight: true,
-      smartypants: true,
-      strikethrough: true,
-      tables: true,
-      with_toc_data: true
-    )
-    markdown.render(contents)
-  end
-
-  def svg(name)
-    root = Middleman::Application.root
-    images_path = config[:images_dir]
-    file_path = "#{root}/source/#{images_path}/#{name}.svg"
-
-    return File.read(file_path) if File.exists?(file_path)
-    "(SVG not found)"
+  def color_element(category)
+    if category == "diatomic nonmetal"
+      "light-blue"
+    elsif category == "noble gas"
+      "light-purple"
+    elsif category == "alkali metal"
+      "red"
+    elsif category == "metalloid"
+      "blue"
+    elsif category == "polyatomic nonmetal"
+      "purple"
+    elsif category == "alkaline earth metal"
+      "orange"
+    elsif category == "post-transition metal"
+      "green"
+    elsif category == "transition metal"
+      "yellow"
+    elsif category == "actinide"
+      "dark-pink"
+    elsif category == "lanthanide"
+      "pink"
+    else
+      "white"
+    end
   end
 end
