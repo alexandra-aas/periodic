0f007b1a97821c6fad29badbb9c6bf1c0e24f478
Ali Aas <ali@thoughtbot.com>
2017-09-08 16:49:51 -0400

    Remove tachyons and finish first pass

--- helpers/application_helpers.rb
+++ helpers/application_helpers.rb
@@ -1,27 +1,2 @@
 module ApplicationHelpers
-  def color_element(category)
-    if category == "diatomic nonmetal"
-      "light-blue"
-    elsif category == "noble gas"
-      "light-purple"
-    elsif category == "alkali metal"
-      "red"
-    elsif category == "metalloid"
-      "blue"
-    elsif category == "polyatomic nonmetal"
-      "purple"
-    elsif category == "alkaline earth metal"
-      "orange"
-    elsif category == "post-transition metal"
-      "green"
-    elsif category == "transition metal"
-      "yellow"
-    elsif category == "actinide"
-      "dark-pink"
-    elsif category == "lanthanide"
-      "pink"
-    else
-      "white"
-    end
-  end
 end
